0eed03a101a1570253c14a17d43d810ffe4c9f20
saracheh <63037246+saracheh@users.noreply.github.com>
2020-04-07 08:46:11 +0200

    Modif process_image.c

--- Microprojet/process_image.c
+++ Microprojet/process_image.c
@@ -46,15 +46,29 @@ static THD_FUNCTION(ProcessImage, arg) {
 	uint8_t *img_buff_ptr;
 	uint8_t image[IMAGE_BUFFER_SIZE] = {0};
 
+	bool send_to_computer = true;
     while(1){
     	//waits until an image has been captured
         chBSemWait(&image_ready_sem);
 		//gets the pointer to the array filled with the last image in RGB565    
 		img_buff_ptr = dcmi_get_last_image_ptr();
 
-		/*
-		*	To complete
-		*/
+		//Extracts only red pixels
+		for(uint16_t i=0; i<(2*IMAGE_BUFFER_SIZE);i+=2)
+		{
+			//Extracts first 5 bits of the first byte
+			//takes nothing from the second byte
+			image[i/2]=(uint8_t)img_buff_ptr[i]&0xF8;
+		}
+
+		if(send_to_computer)
+		{
+			//sends to the computer to the image
+			SendUint8ToComputer(image, IMAGE_BUFFER_SIZE);
+		}
+
+		//invert the bool
+		send_to_computer = !send_to_computer;
     }
 }
 
